11a5635921574d98fe8b4e525c7154cdcd3075c3
Ytallo <ytallolg@gmail.com>
2018-06-25 11:17:41 -0300

    Desenvolvimento da Atividade1

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+const express = require ("express")
+const app = express()
+
+app.get("/hello", (req,res) => {
+	console.log("hello from the other side!")
+	res.send("hello world!!!")
+})
+
+app.listen(3000)
+console.log("server online!")
